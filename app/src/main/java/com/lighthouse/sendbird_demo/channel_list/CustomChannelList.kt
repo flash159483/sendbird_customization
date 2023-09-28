ca95413d8e4a536ac1ec1a54ef393cda9b0ed80f
flash159483 <seungwonjung0313@gmail.com>
2023-09-28 22:08:16 +0900

    Add listener to Custom Channel (#2)

--- app/src/main/java/com/lighthouse/sendbird_demo/channel_list/CustomChannelList.kt
+++ app/src/main/java/com/lighthouse/sendbird_demo/channel_list/CustomChannelList.kt
@@ -4,6 +4,7 @@ import android.content.Intent
 import android.os.Bundle
 import android.view.View
 import android.widget.Toast
+import com.lighthouse.sendbird_demo.channel.CustomChannelActivity
 import com.sendbird.android.channel.GroupChannel
 import com.sendbird.uikit.activities.ChannelActivity
 import com.sendbird.uikit.activities.CreateChannelActivity
@@ -34,8 +35,9 @@ class CustomChannelList : ChannelListFragment() {
                 if (e != null) {
                     Toast.makeText(context, e.toString(), Toast.LENGTH_SHORT).show()
                 } else {
-                    val intent = ChannelActivity.newIntent(
+                    val intent = ChannelActivity.newIntentFromCustomActivity(
                         requireContext(),
+                        CustomChannelActivity::class.java,
                         channel.url
                     )
                     startActivity(intent)
