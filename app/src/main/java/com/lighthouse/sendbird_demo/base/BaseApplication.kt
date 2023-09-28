3c6f6e9b09e34de1fc983cb1dc437c1021a744e8
flash159483 <seungwonjung0313@gmail.com>
2023-09-28 22:07:31 +0900

    Update Deprecated code to new one (#2)

--- app/src/main/java/com/lighthouse/sendbird_demo/base/BaseApplication.kt
+++ app/src/main/java/com/lighthouse/sendbird_demo/base/BaseApplication.kt
@@ -3,15 +3,21 @@ package com.lighthouse.sendbird_demo.base
 import android.app.Application
 import android.util.Log
 import com.lighthouse.sendbird_demo.BuildConfig
-import com.lighthouse.sendbird_demo.CustomFactory
+import com.lighthouse.sendbird_demo.channel.CustomChannelFragment
+import com.lighthouse.sendbird_demo.channel_list.CustomChannelList
 import com.sendbird.android.SendbirdChat
 import com.sendbird.android.exception.SendbirdException
 import com.sendbird.android.handler.InitResultHandler
 import com.sendbird.android.params.InitParams
 import com.sendbird.uikit.SendbirdUIKit
 import com.sendbird.uikit.adapter.SendbirdUIKitAdapter
-import com.sendbird.uikit.fragments.UIKitFragmentFactory
+import com.sendbird.uikit.fragments.ChannelFragment
+import com.sendbird.uikit.fragments.ChannelListFragment
 import com.sendbird.uikit.interfaces.UserInfo
+import com.sendbird.uikit.interfaces.providers.ChannelFragmentProvider
+import com.sendbird.uikit.interfaces.providers.ChannelListFragmentProvider
+import com.sendbird.uikit.model.configurations.UIKitConfig
+import com.sendbird.uikit.providers.FragmentProviders
 
 class BaseApplication : Application() {
     override fun onCreate() {
@@ -21,41 +27,36 @@ class BaseApplication : Application() {
     }
 
     private fun initSendBirdChat() {
-        SendbirdChat.init(
-            InitParams(
-                BuildConfig.SENDBIRD_APPLICATION,
-                applicationContext,
-                useCaching = true
-            ),
-            object : InitResultHandler {
-                override fun onInitFailed(e: SendbirdException) {
-                    Log.i(
-                        "SendBirdInit",
-                        "Called when initialize failed. SDK will still operate properly as if useLocalCaching is set to false."
-                    )
-                }
-
-                override fun onInitSucceed() {
-                    SendbirdChat.connect(BuildConfig.SENDBIRD_USER_ID) { user, e ->
-                        if (user != null) {
-                            if (e != null) {
-                                // Proceed in offline mode with the data stored in the local database.
-                                // Later, connection is made automatically.
-                                // and can be notified through ConnectionHandler.onReconnectSucceeded().
-                            } else {
-                            }
-                        } else {
-                            // Handle error.
-                        }
-                    }
-                    Log.i("SendBirdInit", "Called when initialization is completed.")
-                }
-
-                override fun onMigrationStarted() {
-                    Log.i("SendBirdInit", "Called when there's an update in Sendbird server.")
-                }
+        SendbirdChat.init(InitParams(
+            BuildConfig.SENDBIRD_APPLICATION, applicationContext, useCaching = true
+        ), object : InitResultHandler {
+            override fun onInitFailed(e: SendbirdException) {
+                Log.i(
+                    "SendBirdInit",
+                    "Called when initialize failed. SDK will still operate properly as if useLocalCaching is set to false."
+                )
             }
-        )
+
+            override fun onInitSucceed() {
+                SendbirdChat.connect(BuildConfig.SENDBIRD_USER_ID) { user, e ->
+                    if (user != null) {
+                        if (e != null) {
+                            // Proceed in offline mode with the data stored in the local database.
+                            // Later, connection is made automatically.
+                            // and can be notified through ConnectionHandler.onReconnectSucceeded().
+                        } else {
+                        }
+                    } else {
+                        // Handle error.
+                    }
+                }
+                Log.i("SendBirdInit", "Called when initialization is completed.")
+            }
+
+            override fun onMigrationStarted() {
+                Log.i("SendBirdInit", "Called when there's an update in Sendbird server.")
+            }
+        })
     }
 
     private fun initSendBirdUI() {
@@ -104,6 +105,18 @@ class BaseApplication : Application() {
                 }
             }
         }, this)
-        SendbirdUIKit.setUIKitFragmentFactory(CustomFactory())
+
+        UIKitConfig.groupChannelConfig.enableTypingIndicator = true
+        UIKitConfig.groupChannelConfig.enableReactions = false
+
+        FragmentProviders.channelList = ChannelListFragmentProvider {
+            ChannelListFragment.Builder().withArguments(it).setUseHeader(true)
+                .setCustomFragment(CustomChannelList()).build()
+        }
+
+        FragmentProviders.channel = ChannelFragmentProvider { url, args ->
+            ChannelFragment.Builder(url).withArguments(args).setUseHeader(true)
+                .setCustomFragment(CustomChannelFragment()).build()
+        }
     }
 }
